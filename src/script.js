33a015fba4ad2c45e2ac3042fa7c3111c89800ca
Islam Parchiev <islamparchev006@gmail.com>
2025-04-30 19:11:39 +0300

    add script for tabs

--- src/script.js
+++ src/script.js
@@ -18,12 +18,45 @@ const swiper = new Swiper('.swiper', {
   });
 
 
+    const tabBtns = document.querySelectorAll(".header__media_tab");
+    const tabs = document.querySelectorAll(".header__media_content");
+   
+function showTab(tabId) {
+
+  tabBtns.forEach(button => button.classList.remove('active'));
+  tabs.forEach(panel => panel.classList.remove('active'));
+
+
+  const tabButton = document.querySelector(`button[data-tab="${tabId}"]`);
+  const tabPanel = document.getElementById(tabId);
+
+  if (tabButton && tabPanel) {
+    tabButton.classList.add('active');
+    tabPanel.classList.add('active');
+  }
+}
+
+
+tabBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    const tabId = button.dataset.tab;
+    showTab(tabId);
+  });
+});
+
+
+showTab('tab2');
+
+
   function fixHeaderOnScroll() {
     const header = document.querySelector('.header');
+    const main = document.querySelector('.main');
     if(window.pageYOffset> 300) {
         header.classList.add("fixed");
+        main.classList.add("active");
     }else {
         header.classList.remove("fixed");
+        main.classList.remove("active");
     }
   }
   window.addEventListener("scroll",fixHeaderOnScroll)
