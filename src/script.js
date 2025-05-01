75fe83d7796a296d9af44b7a62f01622f41f0358
Islam Parchiev <islamparchev006@gmail.com>
2025-05-01 09:37:26 +0300

    fix: schedule slider

--- src/script.js
+++ src/script.js
@@ -2,7 +2,6 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
@@ -17,7 +16,43 @@ const swiper = new Swiper('.swiper', {
   
   });
 
-
+  const scheduleSlider = new Swiper('.header__schedule_slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slideClass:"schedule-slide",
+    wrapperClass:"schedule-wrapper",
+    slidesPerView:5,
+    slidesPerGroup:3,
+    slidesPerGroupSkip: 1,
+    spaceBetween:20,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.schedule-navigation__btn--right',
+      prevEl: '.schedule-navigation__btn--left',
+    },
+  
+  });
+  const scheduleSliderFixed = new Swiper('.schedule-list--fixed', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slideClass:"schedule-list__item--fixed",
+    wrapperClass:"schedule-list--wrapper",
+    slidesPerView:6,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type:"fraction"
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.schedule-navigation__btn--left',
+      prevEl: '.schedule-navigation__btn--right',
+    },
+  
+  });
     const tabBtns = document.querySelectorAll(".header__media_tab");
     const tabs = document.querySelectorAll(".header__media_content");
    
@@ -51,7 +86,7 @@ showTab('tab2');
   function fixHeaderOnScroll() {
     const header = document.querySelector('.header');
     const main = document.querySelector('.main');
-    if(window.pageYOffset> 300) {
+    if(window.pageYOffset> 600) {
         header.classList.add("fixed");
         main.classList.add("active");
     }else {
