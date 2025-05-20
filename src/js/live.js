f6b71afe484a05430ee28a771b16d04ec7a18233
Islam Parchiev <islamparchev006@gmail.com>
2025-05-20 10:35:33 +0300

    fix live video

--- src/js/live.js
+++ src/js/live.js
@@ -1,6 +1,7 @@
 const livePlayBtn = document.querySelector(".main-media__play");
 const liveMuteBtn = document.querySelector(".main-media__mute");
-const liveVideo = document.querySelector(".live-video-tag");
+const liveVideo = document.querySelector(".live__main-media");
+const liveVideoTag = document.querySelector(".live-video-tag");
 const fullScreenBtn = document.querySelector(".navigation-bar__fullscreen");
 const liveVideoStatus = document.querySelector(".main-media__status");
 if (liveVideoStatus.getAttribute("data-status") === 'online') {
@@ -14,23 +15,31 @@ livePlayBtn.addEventListener("click", (e) => {
     if (e.target.classList.contains("main-media__play") || e.target.nodeName === "path" || e.target.nodeName === "svg" || e.target.nodeName === "circle" || e.target.nodeName === "rect") {
         if (!livePlayBtn.classList.contains("pause")) {
             livePlayBtn.classList.add("pause");
-            liveVideo.play();
+            liveVideoTag.play();
         } else {
             livePlayBtn.classList.remove("pause");
-            liveVideo.pause();
+            liveVideoTag.pause();
         }
 
     }
 })
 fullScreenBtn.addEventListener('click', () => {
+  if (document.fullscreenElement) {
+    document
+      .exitFullscreen()
+      .then(() => console.log("Document Exited from Full screen mode"))
+      .catch((err) => console.error(err));
+  } else {
     liveVideo.requestFullscreen();
+  }
+
 })
 liveMuteBtn.addEventListener("click", () => {
-    if (liveVideo.muted===true) {
-        liveVideo.muted = false;
+    if (liveVideoTag.muted===true) {
+        liveVideoTag.muted = false;
         liveMuteBtn.classList.remove('muted');
     } else {
-        liveVideo.muted = true;
+        liveVideoTag.muted = true;
         liveMuteBtn.classList.add('muted');
     }
 })
