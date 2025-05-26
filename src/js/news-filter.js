c4a231e09d22caab991c33f3909ccade52890557
Islam Parchiev <IslamParchievv@yandex.ru>
2025-05-26 15:55:21 +0300

    filters

---
+++ src/js/news-filter.js
@@ -0,0 +1,31 @@
+  const tabss = document.querySelectorAll('.tab');
+
+  function filterNews(category) {
+      const newsItems = document.querySelectorAll('li[data-category]');
+      console.log(category);
+      newsItems.forEach(item => item.classList.remove('active'));
+      if (category === 'all') {
+          newsItems.forEach(item => item.classList.add('active'));
+      } else {
+          newsItems.forEach(item => {
+              if (item.dataset.category === category) {
+                  item.classList.add("active");
+              }
+          })
+      }
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+      document.querySelector('[data-tab="all"]').classList.add('active');
+
+      filterNews('all');
+  });
+
+  document.querySelectorAll('.tab').forEach(button => {
+      button.addEventListener('click', () => {
+          document.querySelectorAll('.tab').forEach(btn => btn.classList.remove('active'));
+          button.classList.add('active');
+          const category = button.dataset.tab;
+          filterNews(category);
+      });
+  });
