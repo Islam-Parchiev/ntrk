12b319f5d109dc8d945d47f7bd3c5105bbe2105b
Islam Parchiev <IslamParchievv@yandex.ru>
2025-05-30 16:33:57 +0300

    fix news-item

--- src/js/news-filter.js
+++ src/js/news-filter.js
@@ -3,6 +3,7 @@
       console.log(category);
       newsItems.forEach(item => item.classList.remove('active'));
       newsItems.forEach(item => item.classList.remove("main-news-item"));
+
       if (category === 'all') {
           newsItems.forEach(item => item.classList.add('active'));
       } else {
@@ -14,22 +15,20 @@
       }
       const el = document.querySelector(".news-item.active");
       if (el && el.classList.contains("active")) {
-          el.classList.add("news-item--second")
           el.classList.add("main-news-item");
       }
   }
 
   document.addEventListener('DOMContentLoaded', () => {
-      if (window.location.pathname.split("/").find((item) => item === "tv-program.html")) {
-          filterNews('1');
-          document.querySelector('[data-tab="1"]').classList.add('active');
-      } else {
-          if (document.querySelector('[data-tab="all"]')) {
-              document.querySelector('[data-tab="all"]').classList.add('active');
-          }
-
+      if (document.querySelector('[data-tab="all"]')) {
+          document.querySelector('[data-tab="all"]').classList.add('active');
           filterNews('all');
+      } else {
+          document.querySelector('[data-tab="1"]').classList.add('active');
+          filterNews('1');
       }
+
+
   });
 
   document.querySelectorAll('[data-tab]').forEach(button => {
