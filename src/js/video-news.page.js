d8ad05893fc49581c86318528c7eed1ccb76f1a6
Islam Parchiev <IslamParchievv@yandex.ru>
2025-06-11 16:44:01 +0300

    fix video news

--- src/js/video-news.page.js
+++ src/js/video-news.page.js
@@ -2,13 +2,20 @@ const videoPlayerTag = document.querySelector('.video-player__video-tag');
 const videoPlayer = document.querySelector(".videoPlayer");
 const progressBar = document.querySelector('.video-navigation__progress');
 const playVideoBtn = document.querySelector('.video-player__play-btn');
-const videoNewsMuteBtn = document.querySelector(".video-navigation__sound_btn");
+const videoNewsMuteeBtn = document.querySelector(".video-navigation__sound_btn");
 const videoNewsFullScreenBtn = document.querySelector('.video-navigation__fullscreen');
-const durationEl= document.querySelector('.video-navigation__timeline_duration');
+const durationEl = document.querySelector('.video-navigation__timeline_duration');
 const currentEl = document.querySelector('.video-navigation__timeline_current');
 const videoDescription = document.querySelector(".video-player__media_description");
 const videoNavigation = document.querySelector(".video-navigation");
 const input = document.querySelector(".video-navigation__range-input");
+const videoNavBtn = document.querySelector('.video-navigation__btn');
+
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    seconds = Math.floor(seconds % 60);
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
 noUiSlider.create(progressBar, {
     start: 0,
     connect: [true, false],
@@ -35,58 +42,48 @@ progressBar.noUiSlider.on('slide', values => {
     videoPlayerTag.currentTime = seekTime;
 });
 
-
-window.addEventListener('DOMContentLoaded', () => {
-    durationEl.textContent = formatTime(videoPlayerTag.duration);
-})
 videoPlayerTag.addEventListener('loadedmetadata', () => {
     durationEl.textContent = formatTime(videoPlayerTag.duration);
-    progressBar.noUiSlider.updateOptions({
-        range: {
-            'min': 0,
-            'max': videoPlayerTag.duration
-        }
-    });
 });
+window.addEventListener('DOMContentLoaded', () => {
+    if (videoPlayerTag.readyState > 0) {
+        durationEl.textContent = formatTime(videoPlayerTag.duration);
+    }
+})
+
 
 
 
 
 
 playVideoBtn.addEventListener("click", (e) => {
-    if (e.target.classList.contains("video-navigation__btn") || e.target.nodeName === "path" || e.target.nodeName === "rect" || e.target.nodeName === "svg") {
-        if (playVideoBtn.classList.contains("played")) {
-            playVideoBtn.classList.remove("played");
-            videoDescription.classList.remove('hidden');
-            videoNavigation.classList.add("hidden");
-            videoPlayerTag.pause();
-        } else {
-            playVideoBtn.classList.add("played");
-            videoDescription.classList.add('hidden');
-            videoNavigation.classList.remove("hidden");
-            videoPlayerTag.play();
-        }
 
+    if (playVideoBtn.classList.contains("played")) {
+        playVideoBtn.classList.remove("played");
+        videoDescription.classList.remove('hidden');
+        videoNavigation.classList.add("hidden");
+        videoPlayerTag.pause();
+    } else {
+        playVideoBtn.classList.add("played");
+        videoDescription.classList.add('hidden');
+        videoNavigation.classList.remove("hidden");
+        videoPlayerTag.play();
     }
+
 })
 
-function formatTime(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    seconds = Math.floor(seconds % 60);
-    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
-}
 console.log(videoPlayerTag.volume)
 
-videoNewsMuteBtn.addEventListener("click", () => {
-    if (videoPlayerTag.muted === true && videoNewsMuteBtn.classList.contains("muted")) {
+videoNewsMuteeBtn.addEventListener("click", () => {
+    if (videoPlayerTag.muted === true && videoNewsMuteeBtn.classList.contains("muted")) {
         videoPlayerTag.muted = false;
-        videoNewsMuteBtn.classList.remove("muted");
+        videoNewsMuteeBtn.classList.remove("muted");
         input.value = 0.2;
         videoPlayerTag.volume = 0.2;
         softSlider.noUiSlider.set(0.2);
     } else {
         videoPlayerTag.muted = true;
-        videoNewsMuteBtn.classList.add("muted");
+        videoNewsMuteeBtn.classList.add("muted");
         input.value = 0;
         videoPlayerTag.volume = 0;
         softSlider.noUiSlider.set(0);
@@ -107,48 +104,34 @@ videoNewsFullScreenBtn.addEventListener('click', () => {
 })
 
 
-      let softSlider = document.querySelector('#video-navigation__slider-round');
+let softSlider = document.querySelector('#video-navigation__slider-round');
 
- noUiSlider.create(softSlider, {
-        start: [0],
-        connect: 'lower',
-        step: 0.01,
-        range: {
-          'min': [0.0],
-          'max': [1]
-        }
-      });
- // Из слайдера в input
-      softSlider.noUiSlider.on('update', function (values, handle) {
-        input.value = values[handle];
-        videoPlayerTag.volume = values[handle];
+noUiSlider.create(softSlider, {
+    start: [0],
+    connect: 'lower',
+    step: 0.01,
+    range: {
+        'min': [0.0],
+        'max': [1]
+    }
+});
+// Из слайдера в input
+softSlider.noUiSlider.on('update', function (values, handle) {
+    input.value = values[handle];
+    videoPlayerTag.volume = values[handle];
 
-        if (videoPlayerTag.volume === 0) {
+    if (videoPlayerTag.volume === 0) {
 
-          videoNewsMuteBtn.classList.add('muted');
-          videoPlayerTag.muted = true;
-        } else {
-          videoNewsMuteBtn.classList.remove('muted');
-          videoPlayerTag.muted = false;
-        }
-      });
+        videoNewsMuteeBtn.classList.add('muted');
+        videoPlayerTag.muted = true;
+    } else {
+        videoNewsMuteeBtn.classList.remove('muted');
+        videoPlayerTag.muted = false;
+    }
+});
 
-      // Из input в слайдер
-      input.addEventListener('change', function () {
-        softSlider.noUiSlider.set(this.value);
+// Из input в слайдер
+input.addEventListener('change', function () {
+    softSlider.noUiSlider.set(this.value);
 
-      });
-
-
-       videoNewsMuteBtn.addEventListener("click", () => {
-        if (videoNewsMuteBtn.classList.contains("muted")) {
-          videoPlayerTag.muted = false;
-          videoNewsMuteBtn.classList.remove('muted');
-          softSlider.noUiSlider.set(0.2);
-        } else {
-          videoPlayerTag.muted = true;
-          videoNewsMuteBtn.classList.add('muted');
-          softSlider.noUiSlider.set(0);
-        }
-      })
-
+});
